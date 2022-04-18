adea73a7872cc457e1c0bd8af7f6e1e6264da278
moonctp24 <moonctp24@gmail.com>
2022-04-19 08:41:55 +0900

    Inflearn 스프링 핵심원리 기본편 섹션2

---
+++ SpringLectureCode/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,20 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    //요구사항: 할인 정책은 모든 VIP는 1000원을 할인해주는 고정 금액 할인을 적용
+    private int discountFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade()== Grade.VIP){
+            return discountFixAmount;
+        }
+        else {
+            return 0;
+        }
+    }
+}
---
+++ SpringLectureCode/core/src/main/java/hello/core/member/Grade.java
@@ -0,0 +1,7 @@
+package hello.core.member;
+
+// 요구사항: 회원은 일반과 VIP 두 가지 등급이 있다.
+public enum Grade {
+    BASIC,
+    VIP
+}
