a274e35500e2bbe0c694166e274d5ceadd6f73f7
moonctp24 <moonctp24@gmail.com>
2022-04-20 12:11:02 +0900

    섹션3. 관심사 분리

--- SpringLectureCode/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ SpringLectureCode/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,12 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
+    /* DIP 위반 상태
     private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+     */
+    /* -> DIP 위반하지 않도록 수정 후 */
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
