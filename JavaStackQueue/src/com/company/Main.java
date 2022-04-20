c72279d794b52f227f50b427ede686e3bace1495
moonctp24 <moonctp24@gmail.com>
2022-04-20 23:00:12 +0900

    Stack 두개로 Queue 구현

--- JavaStackQueue/src/com/company/Main.java
+++ JavaStackQueue/src/com/company/Main.java
@@ -1,12 +1,70 @@
 package com.company;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
+    Queue<String> myQueue = new LinkedList<>();
+
+    static Stack st1 = new Stack<>();
+    static Stack st2 = new Stack<>();
+
+    public static <T> void stackPush(T value){
+        st1.push( value);
+    }
+
+    public static <T> T stackPop(){
+        int fixSize = st1.size();
+
+        if(st2.empty()){
+            for(int i=0;i<fixSize;i++){
+                st2.push(st1.pop());
+            }
+        }
+
+        //System.out.println(st2.pop());
+        return (T) st2.pop();
+
+    }
 
     public static void main(String[] args) {
+        /* Stack 두개로 Queue 구현하기 */
+        /*
+        1 2 3 4 5
+        2개 빼고
+        6 7 8
+        4개 뺄거야
+        9
+        3개 다 빼
+        *
+         */
+
+        stackPush(1);
+        stackPush(2);
+        stackPush(3);
+        stackPush(4);
+        stackPush(5);
+
+        stackPop();
+        stackPop();
+
+        stackPush(6);
+        stackPush(7);
+        stackPush(8);
+        stackPop();
+        stackPop();
+        stackPop();
+        stackPop();
+        stackPop();
+
+
+        stackPush(9);
+
+        stackPop();
+        stackPop();
+
+
+        /*
+
         // 스택 정의 및 값 저장
         Stack<String> myStack = new Stack<>();
         myStack.push("0");
@@ -32,5 +90,6 @@ public class Main {
             System.out.println(myQueue.poll());
         }
         System.out.println("Queue =========== End");
+         */
     }
 }
