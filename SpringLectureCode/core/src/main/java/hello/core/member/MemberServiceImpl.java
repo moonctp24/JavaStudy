e9f8a8fc0129fc3616b3946525255ed7af29dab7
moonctp24 <moonctp24@gmail.com>
2022-04-20 12:47:17 +0900

    섹션3. 관심사분리 - AppConfig 생성

--- SpringLectureCode/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ SpringLectureCode/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,7 +1,14 @@
 package hello.core.member;
 
+import hello.core.discount.DiscountPolicy;
+import hello.core.order.OrderService;
+
 public class MemberServiceImpl implements MemberService{
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- SpringLectureCode/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ SpringLectureCode/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,21 +1,19 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    /* DIP 위반 상태
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-     */
-    /* -> DIP 위반하지 않도록 수정 후 */
+    private final MemberRepository memberRepository;
     private DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+        this.memberRepository=memberRepository;
+    }
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
