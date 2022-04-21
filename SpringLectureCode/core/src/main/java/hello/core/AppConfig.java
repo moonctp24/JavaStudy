b09a36f27a44e556026a31c3c4074e999ae3cc27
moonctp24 <moonctp24@gmail.com>
2022-04-21 11:31:46 +0900

    스프링 전환하기

--- SpringLectureCode/core/src/main/java/hello/core/AppConfig.java
+++ SpringLectureCode/core/src/main/java/hello/core/AppConfig.java
@@ -8,22 +8,30 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(MemberRepository());
     }
 
-    private MemoryMemberRepository MemberRepository() {
+    @Bean
+    public MemoryMemberRepository MemberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(MemberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
+        /* 할인 정책 변경 */
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
