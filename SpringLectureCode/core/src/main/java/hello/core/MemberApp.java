e26b8dfbf4a12abe870cc7c75c3ed417873ba4a3
moonctp24 <moonctp24@gmail.com>
2022-04-24 10:33:29 +0900

    스프링으로 전환

--- SpringLectureCode/core/src/main/java/hello/core/MemberApp.java
+++ SpringLectureCode/core/src/main/java/hello/core/MemberApp.java
@@ -5,11 +5,18 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        /* 기존 코드 */
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        /* Switch Spring 코드 */
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         // 회원 생성
         Member member = new Member(1L, "memberA", Grade.VIP);
--- SpringLectureCode/core/src/main/java/hello/core/OrderApp.java
+++ SpringLectureCode/core/src/main/java/hello/core/OrderApp.java
@@ -4,12 +4,20 @@ import hello.core.member.*;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        /* 기존 코드 */
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        /* Switch Spring 코드 */
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
