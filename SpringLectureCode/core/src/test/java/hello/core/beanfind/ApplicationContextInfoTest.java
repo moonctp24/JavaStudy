92c29c11bb79389f7c211d365102c7c155e1b339
moonctp24 <moonctp24@gmail.com>
2022-04-25 08:17:57 +0900

    스프링 빈 조회

---
+++ SpringLectureCode/core/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,36 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("Print All Beans")
+    void findAllBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = "+beanDefinitionName+" object = "+bean);
+        }
+    }
+
+    @Test
+    @DisplayName("Print Application Beans")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            // 스프링 내부적으로 등록된 빈 제외, 내가 직접 추가한 빈만 출력 - BeanDefinition.ROLE_APPLICATION
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+    }
+}
