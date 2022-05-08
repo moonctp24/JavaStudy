63019d56a767f1fc3c07fc2e5c948755821683db
moonctp24 <moonctp24@gmail.com>
2022-05-09 08:54:00 +0900

    빈 중복 등록

--- SpringLectureCode/core/src/main/java/hello/core/AutoAppConfig.java
+++ SpringLectureCode/core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,4 +13,9 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes=Configuration.class))
 // 기존 예제 코드를 지키기 위해서 excludeFilters 추가
 public class AutoAppConfig {
+
+//    @Bean (name = "memoryMemberRepository")
+//    public MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
 }
