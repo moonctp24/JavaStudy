a16e47fa99ae347542ae816b58a0be8c84541c35
moonctp24 <moonctp24@gmail.com>
2022-05-08 13:44:03 +0900

    basePackages - 컴포넌트 기본스캔대상

--- SpringLectureCode/core/src/main/java/hello/core/AutoAppConfig.java
+++ SpringLectureCode/core/src/main/java/hello/core/AutoAppConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes=Configuration.class))
+@ComponentScan(
+//        basePackages = "hello.core.member",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes=Configuration.class))
 // 기존 예제 코드를 지키기 위해서 excludeFilters 추가
 public class AutoAppConfig {
 }
