681675c38e8ccedfefb94c382b2df761fd6ca08e
moonctp24 <moonctp24@gmail.com>
2022-05-15 01:40:30 +0900

    Bean 초기화, 소멸 메소드 지정

--- SpringLectureCode/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ SpringLectureCode/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -5,8 +5,6 @@ public class NetworkClient {
 
     public NetworkClient(){
         System.out.println("생성자 호출, url = "+url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -25,5 +23,16 @@ public class NetworkClient {
     // 서비스 종료시 호출
     public void disconnect(){
         System.out.println("close: "+url);
+    }
+
+    public void init(){
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void close(){
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
